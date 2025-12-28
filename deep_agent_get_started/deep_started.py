d8ad3cadf7c652281177a5f4b089bcb661b939ed
JSYEH <juishuyeh@gmail.com>
2025-12-28 19:59:50 +0800

    feat: 新增深度代理啟動腳本，包含天氣查詢工具及即時更新功能

---
+++ deep_agent_get_started/deep_started.py
@@ -0,0 +1,70 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.tools import tool
+from langchain_core.runnables import RunnableConfig
+from langgraph.graph.state import CompiledStateGraph
+
+from deepagents import create_deep_agent
+from langchain.agents.middleware import AgentMiddleware
+from langgraph.store.memory import InMemoryStore
+from deepagents.backends import CompositeBackend, StateBackend, StoreBackend
+from langgraph.checkpoint.memory import MemorySaver
+from prompt_toolkit import prompt
+
+
+load_dotenv()
+checkpointer = MemorySaver()
+store = InMemoryStore()
+
+
+def make_backend(runtime):
+    return CompositeBackend(
+        default=StateBackend(runtime), routes={"/memories/": StoreBackend(runtime)}
+    )
+
+
+@tool
+def get_weather(city: str) -> str:
+    """Get the weather in a city."""
+    return f"The weather in {city} is sunny."
+
+
+class WeatherMiddleware(AgentMiddleware):
+    tools = [get_weather]
+
+
+llm = init_chat_model("openai:gpt-oss-20b-local")
+
+agent = create_deep_agent(
+    store=store,
+    model=llm,
+    middleware=[WeatherMiddleware()],
+    backend=make_backend,
+    checkpointer=checkpointer,
+)
+
+
+def stream_agent_updates(user_input: str):
+    config = RunnableConfig(configurable={"thread_id": "1"})
+    event = agent.invoke(
+        {"messages": [{"role": "user", "content": user_input}]}, config=config
+    )
+    event["messages"][-1].pretty_print()
+    print()
+    print(
+        "================================================================================"
+    )
+
+
+while True:
+    try:
+        user_input = prompt("User: ")
+        if user_input.strip().lower() in ["exit", "quit", "q"]:
+            print("Exiting chat.")
+            break
+        if user_input.strip() == "":
+            continue
+        stream_agent_updates(user_input)
+    except KeyboardInterrupt:
+        print("\nExiting chat.")
+        break
